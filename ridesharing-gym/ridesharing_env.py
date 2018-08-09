debc45ed612bf2a53eac832bf9f3906669a40044
Michael J. Curry <curry@cs.umd.edu>
2018-08-09 14:56:00 -0400

    added some more methods

--- ridesharing-gym/ridesharing_env.py
+++ ridesharing-gym/ridesharing_env.py
@@ -6,12 +6,15 @@ class RidesharingEnv(gym.Env):
 
     def __init__(self, grid_size, capacity):
         self.action_space = spaces.Discrete(6) # N,S,E,W, center, and reject
-        self.observation_space = spaces.MultiDiscrete(np.tile(capacity, grid_size))
+        self.observation_space = spaces.Tuple((spaces.MultiDiscrete(np.tile(capacity, grid_size)), spaces.MultiDiscrete(np.array([grid_size, grid_size]))))
         # not done
 
     def step(self, action):
         raise NotImplementedError
 
     def reset(self):
+        raise NotImplementedError
+
+    def render(self, mode='human'):
         raise NotImplementedError
 
