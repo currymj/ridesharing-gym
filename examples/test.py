21b7dbd74fe55ba41d5a78512b05325df05f93af
Michael J. Curry <curry@cs.umd.edu>
2018-08-10 09:59:24 -0400

    fixed format string

--- examples/test.py
+++ examples/test.py
@@ -7,5 +7,5 @@ observed_state = env.reset()
 for i in range(10):
     print(observed_state)
     act = env.action_space.sample()
-    print("Randomly chosen action: %d".format(act))
+    print('Randomly chosen action: {}'.format(act))
     observed_state, reward, _, _ = env.step(act)
