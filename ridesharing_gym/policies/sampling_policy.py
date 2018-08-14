9608746e961c19ea523652e3768dda81c94cfc43
Michael J. Curry <curry@cs.umd.edu>
2018-08-14 14:46:54 -0400

    factored out checking move validity

--- ridesharing_gym/policies/sampling_policy.py
+++ ridesharing_gym/policies/sampling_policy.py
@@ -41,6 +41,14 @@ class LogisticSamplingPolicy:
     def alpha(self):
         return scipy.special.expit(self.theta)
 
+    def _valid_move(self, source, cars, action):
+        # assumes destination has capacity
+        act_index = self.grid.get_loc(source, action)
+        if (act_index == -1) or (cars[act_index] == 0):
+            return False
+        else:
+            return True
+
     def _action_probs_array(self, observation):
         probs_array = np.zeros(6)
         cars, request = observation
@@ -52,22 +60,19 @@ class LogisticSamplingPolicy:
             probs_array[0] = 1.0
             return probs_array
 
-
         # check if can match center
-        center_index = self.grid.get_loc(source, 1)
-        if (center_index == -1) or (cars[center_index] == 0):
-            probs_array[0] += self.alpha
-        else:
+        if self._valid_move(source, cars, 1):
             probs_array[1] = self.alpha
+        else:
+            probs_array[0] += self.alpha
 
         # check if can match neighbors
         neighbor_actions = [2,3,4,5]
         for action in neighbor_actions:
-            act_index = self.grid.get_loc(source, action)
-            if (act_index == -1) or (cars[act_index] == 0):
-                probs_array[0] += (1.0 - self.alpha) / 4.0
-            else:
+            if self._valid_move(source, cars, action):
                 probs_array[action] = (1.0 - self.alpha) / 4.0
+            else:
+                probs_array[0] += (1.0 - self.alpha) / 4.0
 
         return probs_array
 
