2745afdf671d344c999551b6156c6da4a36fd5ff
Yuhao-Wan <yuhao.diane.wan@gmail.com>
2018-08-28 08:14:52 +0800

    plot starting map

--- examples/plot.py
+++ examples/plot.py
@@ -7,16 +7,19 @@ env = gym.make('ridesharing-v0')
 vf = valueIteration(env)
 opt_policy = get_policy(env, vf)
 
-observed_state = env.reset();
+observed_state = env.reset()
 observed_state_index = env.b_map[(tuple(observed_state[0]), tuple(observed_state[1]))]
+loc_vec = []
 
-for i in range(10):
-	action = opt_policy[observed_state_index]
+for i in range(30):
+    action = int(opt_policy[observed_state_index])
     _, _, _, _, observed_state_index, loc = env.step(action, detail=True)
-    loc_vec[i] = loc
+    loc_vec.append(loc)
 
 
-counts = [loc_vector.count(x) for x in range(4)]
+counts = [loc_vec.count(x) for x in range(4)]
 counts = np.array(counts)
 count_matrix = counts.reshape(2, 2)
-ax = sns.heatmap(count_matrix)
+starting_loc_map = sns.heatmap(count_matrix)
+starting_loc_fig = starting_loc_map.get_figure()
+starting_loc_fig.savefig("starting_map.png")
